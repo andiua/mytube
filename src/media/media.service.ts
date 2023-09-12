2b98df371087214550f41112c1e5c08cb44a21fe
andiua <andr.babych@gmail.com>
2023-09-12 11:28:35 +0300

    add upload media backend

---
+++ src/media/media.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ensureDir, writeFile } from 'fs-extra';
+import { IMediaResponse } from './media.interface';
+import { path } from 'app-root-path';
+
+@Injectable()
+export class MediaService {
+	async saveMedia(mediaFile: Express.Multer.File, folder = 'default'): Promise<IMediaResponse> {
+		const uploadFolder = `${path}/uploads/${folder}`
+		await ensureDir(uploadFolder);
+
+		await writeFile(`${uploadFolder}/${mediaFile.originalname}`, mediaFile.buffer);
+
+		return {
+			url: `/uploads/${folder}/${mediaFile.originalname}`,
+			name: mediaFile.originalname,
+		};
+	}
+}
