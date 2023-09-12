084a6f0571d664a8e706d4ca1667f7b31a34eef4
andiua <andr.babych@gmail.com>
2023-09-12 09:11:33 +0300

    add comment backend

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	UsePipes,
+	ValidationPipe,
+	HttpCode,
+	Body,
+	Post,
+
+} from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { CurrentUser } from 'src/user/user.decorator';
+import { CommentDto } from './comment.dto';
+import { Auth } from 'src/auth/auth.decorator';
 
 @Controller('comment')
 export class CommentController {
-  constructor(private readonly commentService: CommentService) {}
+	constructor(private readonly commentService: CommentService) {}
+
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post()
+	@Auth()
+	async createComment(@CurrentUser('id') id: string, @Body() dto: CommentDto) {
+		return this.commentService.create(+id, dto);
+	}
 }
