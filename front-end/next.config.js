74e1615004198ea81ccc1b188d69012bf8d21a52
andiua <andr.babych@gmail.com>
2023-09-12 15:31:16 +0300

    initial front-end

---
+++ front-end/next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	poweredByHeader: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+	},
+	images: {
+		domains: ['localhost']
+	},
+	// щоб нект не брав власний локалхост, а той де у нас бекенд
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'http://localhost:4200/api/:path*'
+			},
+			{
+				source: '/uploads/:path*',
+				destination: 'http://localhost:4200/uploads/:path*'
+			}
+		];
+	 }
+}
+
+module.exports = nextConfig
